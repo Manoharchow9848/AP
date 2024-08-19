fb12135f984927d12f6f3f6a94f81503355ccd8c
Manoharchow9848 <=manuchow1.767@gmail.com>
2024-08-19 16:33:37 +0530

    frontend authentication completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
-
-
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Footer from './components/Footer'
+import Protect from './components/Protect'
+import PrivateRoute from './components/PrivateRouutes'
+import DashBoard from './pages/DashBoard'
 function App() {
   
 
   return (
-    <div>
-      hello
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+
+      <Route element={<Protect />}>
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        </Route>
+
+        <Route element={<PrivateRoute />}>
+        <Route path='/dashboard' element={<DashBoard />} />      
+          </Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
