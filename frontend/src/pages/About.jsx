11a8bb59b86284e5ecd8ed174b55065e66c3134f
Manoharchow9848 <=manuchow1.767@gmail.com>
2024-08-25 12:32:23 +0530

    commit

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+
 
 export default function About() {
+
+
+
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="max-w-2xl mx-auto p-3 text-center">
